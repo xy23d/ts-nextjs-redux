b98fba4a0c077c28f91998f9ab6b301e8755dbf6
xy2e45 <ypinku@yahoo.co.jp>
2023-03-20 19:54:48 +0900

    feat: git clone https://github.com/takefumi-yoshii/ts-nextjs-redux.git

---
+++ layouts/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Head, Main, NextScript } from 'next/document'
+// ______________________________________________________
+//
+export default () => (
+  <html>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </html>
+)
